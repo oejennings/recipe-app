b508bad9c2d8004aea21aa92075fb97f9b8f2285
Olivia Jennings <oejennings@users.noreply.github.com>
2024-03-13 13:31:31 -0400

    updated settings

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$#)9oo*b$m5gw-m4ns&4yyd0#=r5lk4g7^!@9dxqd^f%^!3q&8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
