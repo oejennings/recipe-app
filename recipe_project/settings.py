4bf4a164e28e8ae6b3c7955bca67415bd34f67ca
Olivia Jennings <oejennings@users.noreply.github.com>
2024-03-14 16:43:19 -0400

    Update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -78,12 +78,13 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+
+#     }
+# }
 
 
 # Password validation
