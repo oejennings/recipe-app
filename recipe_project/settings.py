469d75cbd4aeb0bc6e02f513927bce44f0cd8512
Olivia Jennings <oejennings@users.noreply.github.com>
2024-03-14 15:31:24 -0400

    Update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,6 +141,7 @@ LOGIN_URL='/login/'
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
